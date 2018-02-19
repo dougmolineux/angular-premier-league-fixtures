63c6470a8c7b43204feea17cf208962894242024
Doug Molineux <doug.molineux@gmail.com>
2018-02-19 14:09:17 -0700

    Fixed a bug and added a screenshot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,15 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Premier League Recent Results and Fixtures';
-  matchData = null
+
+  private readonly title = 'Premier League Recent Results and Fixtures';
+  private matchData = null;
+
   constructor(private http: HttpClient) {
-    this.http.get("http://push.api.bbci.co.uk/p?t=morph%3A%2F%2Fdata%2Fbbc-morph-football-scores-match-list-data%2FendDate%2F2018-02-28%2FstartDate%2F2018-02-01%2FtodayDate%2F2018-02-10%2Ftournament%2Fpremier-league%2Fversion%2F2.4.0&c=1").subscribe( bbcData=>{
+    this.http.get("http://push.api.bbci.co.uk/p?t=morph%3A%2F%2Fdata%2F"+
+      "bbc-morph-football-scores-match-list-data%2FendDate%2F2018-02-28%2F"+
+      "startDate%2F2018-02-01%2FtodayDate%2F2018-02-10%2Ftournament%2F"+
+      "premier-league%2Fversion%2F2.4.0&c=1").subscribe( (bbcData: any) => {
       if (!bbcData) {
         console.log("We didn't get any data :(");
         return false;
